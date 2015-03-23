ba5bb329c12709e7ba667769c8014b8e712106e6
Guillaume Jacquet <guillaume.jacquet@appdirect.com>
2015-03-23 08:35:48 -0400

    test

--- src/main/java/com/github/jacquet/sonar/Test.java
+++ src/main/java/com/github/jacquet/sonar/Test.java
@@ -1,11 +1,19 @@
 package com.github.jacquet.sonar;
 
+import java.io.Serializable;
 import java.util.Collections;
+
+import lombok.RequiredArgsConstructor;
 
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 
-public class Test {
+@RequiredArgsConstructor
+public class Test implements Serializable {
+	private static final long serialVersionUID = 8928414860315448801L;
+
+	private SomeService service;
+
 	public Resources<Resource<SomeBean>> test() {
 		Iterable<SomeBean> list = Collections.singletonList(new SomeBean());
 		return Resources.wrap(list);
