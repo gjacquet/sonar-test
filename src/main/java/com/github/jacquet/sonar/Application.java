5e6d7e8c0c9ccdec5e0f0265ebbe23c9bc3198dd
Guillaume Jacquet <guillaume.jacquet@appdirect.com>
2015-03-23 08:46:05 -0400

    g

--- src/main/java/com/github/jacquet/sonar/Application.java
+++ src/main/java/com/github/jacquet/sonar/Application.java
@@ -2,9 +2,20 @@ package com.github.jacquet.sonar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
+	@Bean
+	public SomeService someService() {
+		return new SomeService();
+	}
+
+	@Bean
+	public Test test() {
+		return new Test();
+	}
+
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
 	}
--- src/main/java/com/github/jacquet/sonar/Test.java
+++ src/main/java/com/github/jacquet/sonar/Test.java
@@ -1,11 +1,14 @@
 package com.github.jacquet.sonar;
 
+import java.io.Serializable;
 import java.util.Collections;
 
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 
-public class Test {
+public class Test implements Serializable {
+	private Application application = new Application();
+
 	public Resources<Resource<SomeBean>> test() {
 		Iterable<SomeBean> list = Collections.singletonList(new SomeBean());
 		return Resources.wrap(list);
